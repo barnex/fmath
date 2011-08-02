d0ff838ebcbd746458c4504d6a2607955c1d27d8
Arne <Arne.Vansteenkiste@UGent.be>
2011-08-02 22:47:05 +0200

    Cleanup

--- sqrtf.go
+++ sqrtf.go
@@ -1,4 +1,4 @@
-// Copyright 2009 The Go Authors. All rights reserved.
+// Copyright 2011 Arne Vansteenkiste. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -6,13 +6,7 @@ package fmath
 
 import "math"
 
-// Sqrtf returns the square root of x.
-//
-// Special cases:
-//	Sqrtf(+Inf) = +Inf
-//	Sqrtf(±0) = ±0
-//	Sqrtf(x < 0) = NaN
-//	Sqrtf(fNaN) = NaN
+// Returns the square root of x.
 func Sqrtf(x float32) float32 {
 	return float32(math.Sqrt(float64(x)))
 }
