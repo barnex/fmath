b0a25010666a8d915d88b5809035a4fa42ff2c55
Arne <Arne.Vansteenkiste@UGent.be>
2011-08-03 20:24:57 +0200

    added functions of 2 variables

---
+++ copysignf.go
@@ -0,0 +1,12 @@
+// Copyright 2011 Arne Vansteenkiste. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package fmath
+
+import "math"
+
+// float32 Copysignf
+func Copysignf(x, y float32) float32 {
+	return float32(math.Copysign(float64(x), float64(y)))
+}
---
+++ powf.go
@@ -0,0 +1,12 @@
+// Copyright 2011 Arne Vansteenkiste. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package fmath
+
+import "math"
+
+// float32 Powf
+func Powf(x, y float32) float32 {
+	return float32(math.Pow(float64(x), float64(y)))
+}
