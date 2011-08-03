f783a7a143e422f5be3e8ddf0e9615007ce3b96c
Arne <Arne.Vansteenkiste@UGent.be>
2011-08-03 20:03:21 +0200

    ready for 2-arg code generation?

--- codegen.bash
+++ codegen.bash
@@ -1,11 +1,20 @@
 #! /bin/bash
-funcs='Acos Acosh Asin Asinh Atan  Atanh Cbrt Ceil  Cos Cosh Erf Erfc Exp Exp2 Expm1 Fabs Floor Fmax Fmin Gamma J0 J1 Log Log10 Log1p Log2 Logb Sin Sinh Sqrt Tan Tanh Trunc Y0 Y1'
 
-for f in $funcs; do
+# float32 -> float32 functions 
+funcs1='Acos Acosh Asin Asinh Atan  Atanh Cbrt Ceil  Cos Cosh Erf Erfc Exp Exp2 Expm1 Fabs Floor Fmax Fmin Gamma J0 J1 Log Log10 Log1p Log2 Logb Sin Sinh Sqrt Tan Tanh Trunc Y0 Y1'
+
+for f in $funcs1; do
 	file=$(echo $f | tr '[A-Z]' '[a-z]')'f'.go
-	sed "s/XXXX/$f/g" template > $file
+	sed "s/XXXX/$f/g" template1 > $file
 	#git add $file
 done
 
+# float32, float32 -> float32 functions
+funcs2='Atan2'
+for f in $funcs2; do
+	file=$(echo $f | tr '[A-Z]' '[a-z]')'f'.go
+	sed "s/XXXX/$f/g" template2 > $file
+	git add $file
+done
 
-#Atan2 Copysign Fdim Fmod Frexp Hypot Inf IsInf IsNaN Ilogb Jn Ldexp Lgamma Modf NaN Nextafter Pow Pow10 Remainder Signbit Sincos  Yn fmaxf fminf
+#Copysign Fdim Fmod Frexp Hypot Inf IsInf IsNaN Ilogb Jn Ldexp Lgamma Modf NaN Nextafter Pow Pow10 Remainder Signbit Sincos  Yn fmaxf fminf
