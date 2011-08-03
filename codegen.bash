69c5309f963d54b5e858b6f5c3a8790551b64db6
Arne <Arne.Vansteenkiste@UGent.be>
2011-08-03 12:14:51 +0200

    codegen for float32 -> float32 funcs

--- codegen.bash
+++ codegen.bash
@@ -1,7 +1,11 @@
 #! /bin/bash
-funcs='Acos Acosh Asin Asinh Atan Atan2 Atanh Cbrt Ceil Copysign Cos Cosh Erf Erfc Exp Exp2 Expm1 Fabs Fdim Float32bits Float32frombits Float64bits Float64frombits Floor Fmax Fmin Fmod Frexp Gamma Hypot Ilogb Inf IsInf IsNaN J0 J1 Jn Ldexp Lgamma Log Log10 Log1p Log2 Logb Modf NaN Nextafter Pow Pow10 Remainder Signbit Sin Sincos Sinh Sqrt Tan Tanh Trunc Y0 Y1 Yn'
+funcs='Acos Acosh Asin Asinh Atan  Atanh Cbrt Ceil  Cos Cosh Erf Erfc Exp Exp2 Expm1 Fabs Floor Fmax Fmin Gamma J0 J1 Log Log10 Log1p Log2 Logb Sin Sinh Sqrt Tan Tanh Trunc Y0 Y1'
 
 for f in $funcs; do
-		file=$(echo $f | tr '[A-Z]' '[a-z]')
-	sed "s/XXXX/$f/g" template > $file.go
+	file=$(echo $f | tr '[A-Z]' '[a-z]')'f'.go
+	sed "s/XXXX/$f'f'/g" template > $file
+	rm $file
 done
+
+
+#Atan2 Copysign Fdim Fmod Frexp Hypot Inf IsInf IsNaN Ilogb Jn Ldexp Lgamma Modf NaN Nextafter Pow Pow10 Remainder Signbit Sincos  Yn
