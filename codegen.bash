eae0ec7b12939abad8c2f6f18f8da278e189f7d1
panmari <smmuzi@gmail.com>
2015-01-07 15:23:01 +0100

    Formatting fixes.

--- codegen.bash
+++ codegen.bash
@@ -4,7 +4,7 @@
 
 
 # float32 -> float32 functions 
-funcs1='Acos Acosh Asin Asinh Atan  Atanh Cbrt Ceil  Cos Cosh Erf Erfc Exp Exp2 Expm1 Floor Gamma J0 J1 Log Log10 Log1p Log2 Logb Sin Sinh Sqrt Tan Tanh Trunc Y0 Y1'
+funcs1='Acos Acosh Asin Asinh Atan Atanh Cbrt Ceil Cos Cosh Erf Erfc Exp Exp2 Expm1 Floor Gamma J0 J1 Log Log10 Log1p Log2 Logb Sin Sinh Sqrt Tan Tanh Trunc Y0 Y1'
 
 for f in $funcs1; do
 	file=$(echo $f | tr '[A-Z]' '[a-z]')'f'.go
