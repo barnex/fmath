bbec39db7821e1243856a9ade4e9abd3a026d717
panmari <smmuzi@gmail.com>
2015-01-07 15:22:45 +0100

    Adapted tests to work nicely with go test
    And fixed tests.

--- sqrtf_test.go
+++ sqrtf_test.go
@@ -1,13 +1,24 @@
-package fmath_test
+package fmath
 
 import (
-	"fmath"
 	"testing"
 )
 
 func TestSqrtf(t *testing.T) {
-	f := fmath.Sqrtf(2)
+	f := Sqrtf(2)
 	if f != 1.41421356237 {
 		t.Fatal("Sqrtf(2):", f)
+	}
+}
+
+func BenchmarkSqrtfAssembly(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Sqrtf(2)
+	}
+}
+
+func BenchmarkSqrtfGo(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Sqrt(2)
 	}
 }
